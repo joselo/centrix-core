55adac174dea3561cb09c90b970d20b75b44419f
Jose Carrion <joseloc@gmail.com>
2026-05-15 16:27:04 -0500

    feat: add validators for Ecuadorian identification numbers and electronic document formats

---
+++ lib/document_number_validator.ex
@@ -0,0 +1,22 @@
+defmodule BillingCore.DocumentNumberValidator do
+  @moduledoc """
+  Validates the format of Ecuadorian electronic document numbers.
+  The format is: XXX-XXX-XXXXXXXXX (Establishment-EmissionPoint-Sequential).
+  """
+
+  @regex ~r/^\d{3}-\d{3}-\d{9}$/
+
+  @doc """
+  Returns true if the document number is valid, false otherwise.
+  """
+  def valid?(number) when is_binary(number) do
+    Regex.match?(@regex, number)
+  end
+
+  def valid?(_), do: false
+
+  @doc """
+  Returns the regex used for validation.
+  """
+  def regex, do: @regex
+end
