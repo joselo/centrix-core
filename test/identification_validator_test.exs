9aec4576d34967b76dbbfa1b6b5a8e4546e5e07d
Jose Carrion <joseloc@gmail.com>
2026-05-25 08:08:56 -0500

    add validators

---
+++ test/identification_validator_test.exs
@@ -0,0 +1,30 @@
+defmodule BillingCore.IdentificationValidatorTest do
+  use ExUnit.Case, async: true
+  alias BillingCore.IdentificationValidator
+
+  describe "valid_identification?/2" do
+    test "validates cedula" do
+      assert IdentificationValidator.valid_identification?(:cedula, "1710034065") == :ok
+      assert {:error, _} = IdentificationValidator.valid_identification?(:cedula, "1710034066")
+    end
+
+    test "validates ruc" do
+      assert IdentificationValidator.valid_identification?(:ruc, "1710034065001") == :ok
+      assert {:error, _} = IdentificationValidator.valid_identification?(:ruc, "1710034065000")
+    end
+
+    test "validates consumidor_final (to be implemented)" do
+      assert IdentificationValidator.valid_identification?(:consumidor_final, "9999999999999") == :ok
+      assert {:error, :invalid_consumidor_final} = IdentificationValidator.valid_identification?(:consumidor_final, "1234567890123")
+    end
+
+    test "validates pasaporte (to be implemented)" do
+      assert IdentificationValidator.valid_identification?(:pasaporte, "ABC123456") == :ok
+      assert IdentificationValidator.valid_identification?(:pasaporte, "any_string") == :ok
+    end
+
+    test "returns error for invalid input" do
+      assert IdentificationValidator.valid_identification?(:cedula, 123) == {:error, :invalid_input}
+    end
+  end
+end
