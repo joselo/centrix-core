d6232df0fd955b20ec2bbee453ec1abcd5574d8e
Jose Carrion <joseloc@gmail.com>
2026-06-04 15:47:56 -0500

    feat: Add SRI health check endpoint

--- lib/sri_client/ws/client_behaviour.ex
+++ lib/sri_client/ws/client_behaviour.ex
@@ -1,5 +1,6 @@
 defmodule BillingCore.Ws.ClientBehaviour do
   @moduledoc false
   @callback post(String.t(), String.t()) :: tuple()
+  @callback post(String.t(), String.t(), keyword()) :: tuple()
   @callback put(String.t(), String.t()) :: tuple()
 end
